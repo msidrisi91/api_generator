a94beaa8fb9e61413dfbf2fd5e3f7d494f1fb7a9
msidrisi91 <mohdshaheem91@gmail.com>
2020-09-28 16:37:34 +0530

    model generator updated

--- generators/model_gen.js
+++ generators/model_gen.js
@@ -9,18 +9,16 @@ function generate(a) {
     var tab = '    ';
     var tab2 = tab + tab;
     var tab3 = tab2 + tab;
-    // console.log(fields);
-    // for(var i = 0; i < parseInt(a['n']); i++){
-    //     console.log('\nheya\n');
-    //     console.log(fields[i]);
-    // }
+
     Object.keys(fields).forEach((f) => {
-        // console.log(f +' => ' + fields[f])
         if(f.includes('name')){
             schema = schema + tab2 + fields[f] + ': {\n';
-        }
-        else if(f.includes('type')){
+        } else if(f.includes('type')){
             schema = schema + tab3 + 'type: ' + fields[f] + ',\n';
+        } else if(f.includes('required') && fields[f]) {
+            schema = schema + tab3 + 'required: true\n' + tab2 + '},\n';
+        } else if(f.includes('default') && fields[f]){
+            schema = schema + tab3 + 'default: '+fields[f]+'\n' + tab2 + '},\n';
         }
     });
 }
